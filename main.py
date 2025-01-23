be80492d01d89e979a9ff248a83cdca2b26cda73
youngryul <76697876+youngryul@users.noreply.github.com>
2025-01-23 08:32:00 +0000

    Day4 계산기 개발

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-def get_yearly_revenue(mothly_revenue):
-  return mothly_revenue * 12
+first = 0
+second = 0
+operation = ''
+result = 0
 
-def get_yearly_expenses(mothly_expenses):
-  return mothly_expenses * 12
+def calc():
+  print("Choose a number : ")
+  first = int(input())
+  
+  print("Choose another one : ")
+  second = int(input())
+  
+  print('''Choose an operation
+              Options are: +, -, * or /.
+              Write 'exit' to finish.''')
+  operation = input()
 
-def get_tax_amount(profit):
-  if profit > 100000:
-    return profit * profit * 0.25
-  else:
-    return profit * profit * 0.15
-
-def apply_tax_credits(tax_amount, tax_credits):
-  return tax_amount * tax_credits
+  return first, second, operation
 
 
+while True:
+  first, second, operation = calc()
 
-monthly_revenue = 5500000
-monthly_expenses = 2700000
-tax_credits = 0.01
-
-yearly_revenue = get_yearly_revenue(monthly_revenue)
-yearly_expenses = get_yearly_expenses(monthly_expenses)
-
-profit = yearly_revenue - yearly_expenses
-
-tax_amount = get_tax_amount(profit)
-
-final_tax_amount = tax_amount - apply_tax_credits(tax_amount, tax_credits)
-
-print(f"Your tax bill is: ${final_tax_amount}")
+  if operation == 'exit':
+    break
+  
+  if(operation == '+'):
+    result = first + second
+  elif(operation == '-'):
+    result = first - second
+  elif(operation == '*'):
+    result = first * second
+  elif(operation == '/'):
+    result = first / second
+    
+  print("Result : ", result)
